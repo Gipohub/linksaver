a577a58e403460833b32794f08e3fdb29dfa20f9
Gipohub <Shestyara10@gmail.com>
2025-04-21 15:37:32 +0300

    create server's maket

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net"
 
+	linksaver "github.com/Gipohub/linksaver/proto"
+	"github.com/Gipohub/linksaver/server"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	linksaver.RegisterLinkSaverServer(s, server.New())
+
+	log.Println("LinkSaver gRPC server is running on :50051")
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
--- server/server.go
+++ server/server.go
@@ -1,1 +1,65 @@
 package server
+
+import (
+	"context"
+	"log"
+	"sync"
+
+	linksaver "github.com/Gipohub/linksaver/proto"
+)
+
+type linkSaverServer struct {
+	linksaver.UnimplementedLinkSaverServer
+	mu    sync.Mutex
+	pages map[string]map[string]struct{} // username -> set of URLs
+}
+
+func New() *linkSaverServer {
+	return &linkSaverServer{
+		pages: make(map[string]map[string]struct{}),
+	}
+}
+
+func (s *linkSaverServer) Save(ctx context.Context, req *linksaver.SaveRequest) (*linksaver.SaveResponse, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	log.Printf("i am in saveGRPCserver ")
+	if s.pages[req.Username] == nil {
+		s.pages[req.Username] = make(map[string]struct{})
+	}
+	s.pages[req.Username][req.Url] = struct{}{}
+
+	return &linksaver.SaveResponse{Status: "ok"}, nil
+}
+
+func (s *linkSaverServer) IsExists(ctx context.Context, page *linksaver.Page) (*linksaver.ExistsResponse, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	_, exists := s.pages[page.Username][page.Url]
+	return &linksaver.ExistsResponse{Exists: exists}, nil
+}
+
+func (s *linkSaverServer) PickRandom(ctx context.Context, user *linksaver.User) (*linksaver.Page, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	for url := range s.pages[user.Username] {
+		return &linksaver.Page{Url: url, Username: user.Username}, nil
+	}
+	return nil, nil
+}
+
+func (s *linkSaverServer) PickAll(ctx context.Context, user *linksaver.User) (*linksaver.PageList, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	list := &linksaver.PageList{}
+	for url := range s.pages[user.Username] {
+		list.Pages = append(list.Pages, &linksaver.Page{
+			Url:      url,
+			Username: user.Username,
+		})
+	}
+	return list, nil
+}
